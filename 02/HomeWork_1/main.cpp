b9e1d51b000b7aa32141519db8dc1de64e3e4dbf
Denis Stepulenok <super.denis@gmail.com>
2015-10-15 22:13:12 +0300

    Занятие 2 - Домашняя работа

---
+++ 02/HomeWork_1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Точка
+struct Point {
+  double x, y;
+};
+
+double sqr(double x) {
+  return x * x;
+}
+
+double dist(Point a, Point b) {
+  return
+    sqrt(sqr(a.x - b.x) +
+         sqr(a.y - b.y));
+}
+
+typedef Point Vector;
+
+// Сумма
+Point sum(Vector A, Vector B) {
+  // TODO: Реализовать
+  return R;
+}
+
+// Разность
+Point sub(Vector A, Vector B) {
+  // TODO: Реализовать
+  return R;
+}
+
+// Скалярное произведение векторов
+double dotProduct(Vector A, Vector B) {
+  // TODO: Реализовать
+}
+
+int main() {
+  Point a = {1.2, 1.3},
+        b = {10.2, 3.3};
+  cout << dist(a, b) << endl;
+
+  // Тестирование
+  Point c1 = sum(a, b);
+  Point c2 = sub(a, b);
+  double p = dotProduct(a, b);
+
+  return 0;
+}
