2e0e3e423cb34919d8a02d75aa6143fe2af17453
Denis Stepulenok <super.denis@gmail.com>
2015-10-21 20:55:09 +0300

    Занятие 3

--- 03/01_strings_buffer_oveflow/main.cpp
+++ 03/01_strings_buffer_oveflow/main.cpp
@@ -1,24 +1,46 @@
-// Переполнение строки символов
-// Смертный Грех программиста №1 - источник уязвимостей
+/// Переполнение строки символов
+/// ----------------------------
+/// Смертный Грех программиста №1 - источник уязвимостей
 #include <iostream>
 #include <cstring> // strcmp
+#include <iomanip> // setw
 
 using namespace std;
 
+// Два способа ограничить ввод:
+// Использовать string вместо char[SIZE]
+//#define STRING
+// Использовать setw(SIZE) при вводе
+//#define SETW
+
 int main() {
   // Строка для ввода пароля
+#ifdef STRING
+  string password;
+#else
   char password[8];
+#endif
   // Корректный ли пароль?
   bool passwordCorrect = false; // 0 - false
 
   cout << "Enter your password: ";
+  // Ограничиваем количество вводимых символов
+#ifdef SETW
+  cin >> setw(8) >> password;
+#else
   cin >> password;
+#endif // SETW
 
   // Ввод с клавиатуры
   // Ввод из файла
   // Получение пакета из сети
+#ifdef STRING
+  if(password == "123")
+    passwordCorrect = true;
+#else
   if(!strcmp(password, "123"))
     passwordCorrect = true;
+#endif
 
   if(passwordCorrect)
     cout << "[OK] Password correct!" << endl;
---
+++ 03/08_array_sort/main.cpp
@@ -0,0 +1,75 @@
+/// Сортировка массива
+/// ------------------
+#include <iostream>
+#include <algorithm> // sort Алгоритмы из STL
+
+using namespace std;
+
+// Пример массива для сортировки
+//-->
+const int N = 6;
+int A[N] = {1, 10, -2, 5, 6, 11};
+//<--
+
+// Выводим массив
+void show() {
+  for(int i = 0; i < N; ++i)
+    cout << A[i] << " ";
+  cout << endl;
+}
+
+int main() {
+  show();
+  // Задание: отсортировать массив
+  // {-2, 10, 1, 5, 6, 11}
+  // {-2, [ 1, 10, 5, 6, 11 ] }
+  // {-2, 1, [5, 10, 6, 11 ] }
+  int changes;
+
+  /* do {
+     changes = 0; // нет перестановок в массиве
+
+     for(int i = 0; i < N - 1; ++i) {
+       if(A[i] > A[i + 1]) {
+         // SWAP
+         //int temp = A[i];
+         //A[i] = A[i+1];
+         //A[i+1] = temp;
+         //A[i] ^= A[i+1] ^= A[i] ^= A[i+1];
+         cout << "swap(" << A[i] << ", " << A[i + 1] << ")" << endl;
+         swap(A[i], A[i + 1]);
+         show();
+         changes = 1;
+       }
+     }
+     cout << "  changes " << changes << " -> ";
+     show();
+   } while(changes); */
+
+  for(int i = 0; i < N - 1; ++i)
+    for(int j = i + 1; j < N; ++j)
+      if(A[i] > A[j]) {
+        cout << "swap(" << A[i] << ", " << A[j] << ")" << endl;
+        swap(A[i], A[j]);
+        show();
+      }
+
+  // -----
+  // | 1 | 0
+  // -----
+  // | 3 | 1
+  // -----
+  // | 5 | 2
+  // -----
+  // | 6 | 3  <-- i
+  // -----
+  // | 7 | 4
+  // -----
+
+  // Снова вывести на экран
+  for(int i = 0; i < N; ++i)
+    cout << A[i] << " ";
+  cout << endl;
+
+  return 0;
+}
